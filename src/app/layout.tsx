7b641bb80a226e19e6b5599c2b6238c567198270
annguyen1909 <garenalord1234@gmail.com>
2026-06-07 16:30:30 +0700

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,9 +109,13 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: '/favicon.ico', sizes: '48x48', type: 'image/x-icon' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
     ],
+    shortcut: [{ url: '/favicon.ico', type: 'image/x-icon' }],
     apple: [{ url: '/apple-touch-icon.png', type: 'image/png' }],
     other: [
       {
